170edaacb70c1209b1bcb8cdba2b61805b7d65d4
Shreeram N S <nsshreeram@gmail.com>
2024-05-28 00:10:54 +0530

    custom hook useCurrencyInfo

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
