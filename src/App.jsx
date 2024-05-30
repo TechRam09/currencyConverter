982ecf1ace6572726dcb98d4e08412095911f998
Shreeram N S <nsshreeram@gmail.com>
2024-05-31 00:06:47 +0530

    Update App.jsx
    imported HOOK

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import InputBox from './components/InputBox'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
@@ -6,7 +7,10 @@ import './App.css'
 function App() {
  
   const [amount,setAmount] =useState(0)
-
+  const [from,setFrom] = useState('usd')
+  const [to,setTo] = useState('inr')
+  const [result, setReseult] = useState(0)
+  
   return (
     <>
       
