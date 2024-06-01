50ce8989a32f573720a4c0da49a15dadbde2b795
Shreeram N S <nsshreeram@gmail.com>
2024-06-01 23:38:00 +0530

    Added Convert method

--- src/App.jsx
+++ src/App.jsx
@@ -21,35 +21,41 @@ function App() {
     setResult(amount)
     setAmount(result)
   }
+    
+    const convert = () => {
+        setResult(amount * currencyInfo[to])
+    }
 
     const BackgroundImage = 'https://img.freepik.com/free-vector/gradient-cryptocurrency-concept_23-2149215736.jpg'
   
     return (
+        
         <div
             className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
             style={{
                 backgroundImage: `url('${BackgroundImage}')`,
             }}
         >
+            
             <div className="w-full">
                 <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
-                           
+                            convert()
                         }}
                     >
                         <div className="w-full mb-1">
                             <InputBox
                                 label="From"
-                                
+                       
                             />
                         </div>
                         <div className="relative w-full h-0.5">
                             <button
                                 type="button"
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                                
+                         
                             >
                                 swap
                             </button>
@@ -57,7 +63,7 @@ function App() {
                         <div className="w-full mt-1 mb-4">
                             <InputBox
                                 label="To"
-                                
+                            
                             />
                         </div>
                         <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
